9f73525e7043da44baf7a3898473ebc2c2e04b01
Ptaal <pouria.ei@gmail.com>
2017-07-20 16:58:54 -0700

    testing3

--- assign4/shinyApp/server.R
+++ assign4/shinyApp/server.R
@@ -26,79 +26,44 @@ shinyServer(
         partition <- reactive({
             partition <- input$dataPartition
         })
-        output$table <- renderTable({
+        output$text4 <- renderText({
             if(iristext2() == "iris") {
                 outcome = "Species"
             }
             else {
                 outcome = "spray"
             }
-            #return(outcome)
             inTrain <- createDataPartition(
                 y = dataset()[[outcome]], p = partition(), 
                 list = FALSE)
-            #return(length(inTrain))
-            #training <- dataset()[inTrain, ]
             training <- reactive({
                 dataset()[inTrain, ]
             })
-            return(head(training()))
-            #testing <- reactive({
-                #dataset()[-inTrain, ]
-            #})
-            #testing <- dataset()[-inTrain, ]
-            #predictors <- reactive({
-                #names(
-                    #dataset()[-which(names(dataset()) == outcome)]
-                    #)
-            #})
-            #set.seed(123)
-            #return(training()[[1]])
-            #return(predictors())
-            #predictors <- names(
-                #dataset()[-which(names(dataset()) == outcome)])
-            #return(predictors)
-            #set.seed(123)
-            #train_control <- trainControl(
-                #method = "cv", number = 10, 
-                #savePredictions = 'final', classProbs = TRUE)
-            #set.seed(123)
-            #model_fit <- train(training[[ ,predictors]], 
-                               #training[[ ,outcome]], 
-                               #method = models(),
-                               #trControl = train_control)
+            testing <- reactive({
+                dataset()[-inTrain, ]
+            })
+            predictors <- reactive({
+                names(
+                    dataset()[-which(names(dataset()) == outcome)]
+                    )
+            })
+            set.seed(123)
+            train_control <- trainControl(
+                method = "cv", number = 10, 
+                savePredictions = 'final', classProbs = TRUE)
+            set.seed(123)
+            model_fit <- train(training()[predictors()], 
+                               factor(training()[[outcome]]), 
+                               method = models(),
+                               trControl = train_control
+                               )
+            return(model_fit$results[['Accuracy']])
             #output$text5 <- renderText({
                 #model_fit$results['Accuracy']})
             #return(model_fit)
-        }, align = 'l')
-        #output$table <- renderTable(training)
-        #inTrain <- createDataPartition(
-         #   y = models$outcome, p = 0.3, list = FALSE
-        #)
-        
+        })
     }
 )
-
-
-
-#diabetesRisk <- function(glucose) glucose / 200
-
-#library(shiny)
-#library(datasets)
-#library(caret)
-#data()
-#shinyServer(
-    #function(input, output) {
-        #data(iris)
-        #output$text1 <- renderText({input$text1})
-        #output$text2 <- renderText({input$text2})
-        #output$text3 <- renderText({
-            #if(input$goButton == 0) "You have not pressed the button"
-            #else if(input$goButton == 1) "You pressed it once"
-            #else "OK quit pressing it"
-        #})
-    #}
-#)
 
 
 
--- assign4/shinyApp/ui.R
+++ assign4/shinyApp/ui.R
@@ -35,28 +35,11 @@ shinyUI(fluidPage(
                textOutput('text4'), 
                p('Output text5'), 
                textOutput('text5')
-        ), 
-        column(4, offset = .5, 
-               tableOutput('table')
+               #p('Output numeric1')
         )
+        #column(4, offset = .5, 
+               #tableOutput('table')
+        #)
         #tableOutput('table')
     )
 ))
-
-
-#shinyUI(pageWithSidebar(
-    #headerPanel("Hello Shiny!"), 
-    #sidebarPanel(
-        #textInput(inputId = "text1", label = "Input Text1"), 
-        #textInput(inputId = "text2", label = "Input Text2"), 
-        #actionButton("goButton", "Go!")
-    #), 
-    #mainPanel(
-        #p('Output text1'),
-        #textOutput('text1'), 
-        #p('Output text2'), 
-        #textOutput('text2'), 
-        #p('Output text3'), 
-        #textOutput('text3')
-    #)
-#))
